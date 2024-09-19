72837b36613a63b8a5c1bf6da75ac1add83358bc
darthleonard <leolinuxmx@gmail.com>
2024-09-18 22:31:46 -0600

    working on android and web

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'com.darthleonard.edaddemiperro',
   appName: 'EdadDeMiPerro',
   webDir: 'www',
   bundledWebRuntime: false
