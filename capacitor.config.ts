51d04de6349923c22f14e03a2a4e8d2b3938d185
darthleonard <leolinuxmx@gmail.com>
2026-08-15 18:28:39 -0600

    migrating angular 20, ionic 8 cap 8

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,10 +1,9 @@
-import { CapacitorConfig } from '@capacitor/cli';
+import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'com.darthleonard.edaddemiperro',
   appName: 'EdadDeMiPerro',
-  webDir: 'www',
-  bundledWebRuntime: false
+  webDir: 'www/browser',
 };
 
 export default config;
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -5,6 +5,7 @@ import { AlertController } from '@ionic/angular';
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
+  standalone: false,
 })
 export class HomePage {
   constructor(private alertController: AlertController) {}
